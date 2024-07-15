357eea9ea3f42e3687b54686d417bc5abf67d3e5
Daniel Šoltýs <76617477@cuni.cz>
2024-07-15 12:42:14 +0200

    Added Register views and controller

---
+++ CSharpChallenge/CSharpChallenge/Controllers/RegisterController.cs
@@ -0,0 +1,30 @@
+﻿using CSharpChallenge.Models;
+using CSharpChallenge.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CSharpChallenge.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProcessRegistration(UserModel userModel)
+        {
+            SecurityService securityService = new SecurityService();
+
+            if (securityService.AreValidRegistrationDetails(userModel))
+            {
+                UsersDAO usersDAO = new UsersDAO();
+                usersDAO.CreateUser(userModel);
+                return View("testPage", userModel);
+            }
+            else
+            {
+                return View("InvalidRegistration", userModel);
+            }
+        }
+    }
+}
