4fce3816ba4a2f9db9f6bcaceebfb4e940f62d57
Daniel Šoltýs <76617477@cuni.cz>
2024-07-15 12:41:11 +0200

    Added Login views and controller

---
+++ CSharpChallenge/CSharpChallenge/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using CSharpChallenge.Models;
+using CSharpChallenge.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CSharpChallenge.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProcessLogin(UserModel userModel)
+        {
+            SecurityService securityService = new SecurityService();
+
+            if (securityService.IsValidLogin(userModel))
+            {
+                return View("ProblemsPage", userModel);
+            }
+            else if (securityService.DoesUserNameExist(userModel))
+            {
+                return View("IncorrectPasswordLogin", userModel);
+            }
+            else
+            {
+                return View("IncorrectUserNameLogin", userModel);
+            }
+        }
+    }
+}
