da4db43375d2b5b3bbf5e29f57cbfc038dccd1f1
Daniel Šoltýs <76617477@cuni.cz>
2024-07-15 12:43:45 +0200

    Changed default page from Home to Login

--- CSharpChallenge/CSharpChallenge/Program.cs
+++ CSharpChallenge/CSharpChallenge/Program.cs
@@ -28,7 +28,7 @@ namespace CSharpChallenge
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Login}/{action=Index}/{id?}");
 
             app.Run();
         }
