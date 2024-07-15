43c019f50a98d449137efcaeda50e8d26e634c62
Daniel Šoltýs <76617477@cuni.cz>
2024-07-15 12:44:15 +0200

    Added UserModel

---
+++ CSharpChallenge/CSharpChallenge/Models/UserModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CSharpChallenge.Models
+{
+    public class UserModel
+    {
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public bool  Admin { get; set; }
+    }
+}
