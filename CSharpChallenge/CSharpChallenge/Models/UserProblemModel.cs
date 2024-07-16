236df6100edda94c12147b5deb5aa65112b1b409
Daniel Šoltýs <76617477@cuni.cz>
2024-07-16 23:32:45 +0200

    Added UserProblem model and DAO

---
+++ CSharpChallenge/CSharpChallenge/Models/UserProblemModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CSharpChallenge.Models
+{
+    public class UserProblemModel
+    {
+        public int Id { get; set; }
+        public int UserID { get; set; }
+        public int ProblemID { get; set; }
+    }
+}
---
+++ CSharpChallenge/CSharpChallenge/Services/UserProblemDAO.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Razor.Language.Extensions;
+using System.Data.SqlClient;
+
+namespace CSharpChallenge.Services
+{
+    public class UserProblemDAO
+    {
+        public bool IsProblemDone(int problemID, int userID)
+        {
+            bool done = false;
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=CSharpChallenges;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+
+            string sqlStatement = "SELECT * FROM dbo.UserProblem WHERE userid = @userid AND problemid = @problemid";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(sqlStatement, connection);
+
+                cmd.Parameters.Add("@userid", System.Data.SqlDbType.Int).Value = userID;
+                cmd.Parameters.Add("@problemid", System.Data.SqlDbType.Int).Value = problemID;
+
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.HasRows)
+                    {
+                        done = true;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            return done;
+        }
+    }
+}
