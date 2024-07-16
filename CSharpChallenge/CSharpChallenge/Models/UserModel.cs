15b95c246e259f3a5eb2e6670d4df0dd5c847e6e
Daniel Šoltýs <76617477@cuni.cz>
2024-07-16 23:34:57 +0200

    Added restrictions to UserModel properties

--- CSharpChallenge/CSharpChallenge/Models/UserModel.cs
+++ CSharpChallenge/CSharpChallenge/Models/UserModel.cs
@@ -1,11 +1,25 @@
-﻿namespace CSharpChallenge.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CSharpChallenge.Models
 {
     public class UserModel
     {
+        [Required]
         public int UserID { get; set; }
+        [Required]
+        [StringLength(40)]
+        [DisplayName("Username:")]
         public string UserName { get; set; }
+        [Required]
+        [StringLength(40, MinimumLength =4)]
+        [DisplayName("Password:")]
         public string Password { get; set; }
+        [Required]
+        [StringLength(80)]
+        [DisplayName("Email:")]
         public string Email { get; set; }
+        [Required]
         public bool  Admin { get; set; }
     }
 }
