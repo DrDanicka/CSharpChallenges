877e9d22eeac26f4e5ff407a0251e088da646108
Daniel Soltys <danielsytlos@gmail.com>
2024-07-24 22:31:01 +0200

    Added uploads folder for solution from user

---
+++ CSharpChallenge/CSharpChallenge/wwwroot/uploads/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        string line1 = Console.ReadLine();
+        string line2 = Console.ReadLine();
+        
+        Console.WriteLine(line1);
+        Console.WriteLine(line2);
+    }
+}
